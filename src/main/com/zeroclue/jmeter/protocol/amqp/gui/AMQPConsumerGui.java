d473e2704569669eea652b5395c4439b4c0d48ab
Sridhar Gunapu <sgunapu@gmail.com>
2013-05-05 23:11:30 -0700

    added     pub/sub : Aggregate sample results to make it more like the JMS sampler     pub: Persistent Message Option to set easily set message type, Use publisher Confirms to fsync messages     sub: Read response, prefetch count

--- src/main/com/zeroclue/jmeter/protocol/amqp/gui/AMQPConsumerGui.java
+++ src/main/com/zeroclue/jmeter/protocol/amqp/gui/AMQPConsumerGui.java
@@ -8,14 +8,18 @@ import org.apache.jorphan.gui.JLabeledTextField;
 
 import com.zeroclue.jmeter.protocol.amqp.AMQPConsumer;
 
+import java.awt.*;
+
 
 public class AMQPConsumerGui extends AMQPSamplerGui {
 
     private static final long serialVersionUID = 1L;
 
     protected JLabeledTextField receiveTimeout = new JLabeledTextField("Receive Timeout");
+    protected JLabeledTextField prefetchCount = new JLabeledTextField("Prefetch Count");
     private final JCheckBox purgeQueue = new JCheckBox("Purge Queue", false);
     private final JCheckBox autoAck = new JCheckBox("Auto ACK", true);
+    private final JCheckBox readResponse = new JCheckBox("Read Response", AMQPConsumer.DEFAULT_READ_RESPONSE);
 
     private JPanel mainPanel;
 
@@ -28,6 +32,11 @@ public class AMQPConsumerGui extends AMQPSamplerGui {
      */
     protected void init() {
         super.init();
+
+        mainPanel.add(readResponse);
+
+        prefetchCount.setPreferredSize(new Dimension(100,25));
+        mainPanel.add(prefetchCount);
 
         mainPanel.add(receiveTimeout);
         mainPanel.add(purgeQueue);
@@ -48,6 +57,8 @@ public class AMQPConsumerGui extends AMQPSamplerGui {
         if (!(element instanceof AMQPConsumer)) return;
         AMQPConsumer sampler = (AMQPConsumer) element;
 
+        readResponse.setSelected(sampler.getReadResponseAsBoolean());
+        prefetchCount.setText(sampler.getPrefetchCount());
         receiveTimeout.setText(sampler.getReceiveTimeout());
         purgeQueue.setSelected(sampler.purgeQueue());
         autoAck.setSelected(sampler.autoAck());
@@ -59,6 +70,8 @@ public class AMQPConsumerGui extends AMQPSamplerGui {
     @Override
     public void clearGui() {
         super.clearGui();
+        readResponse.setSelected(AMQPConsumer.DEFAULT_READ_RESPONSE);
+        prefetchCount.setText(AMQPConsumer.DEFAULT_PREFETCH_COUNT_STRING);
         receiveTimeout.setText("");
         purgeQueue.setSelected(false);
         autoAck.setSelected(true);
@@ -85,9 +98,13 @@ public class AMQPConsumerGui extends AMQPSamplerGui {
 
         super.modifyTestElement(sampler);
 
+        sampler.setReadResponse(readResponse.isSelected());
+        sampler.setPrefetchCount(prefetchCount.getText());
+
         sampler.setReceiveTimeout(receiveTimeout.getText());
         sampler.setPurgeQueue(purgeQueue.isSelected());
         sampler.setAutoAck(autoAck.isSelected());
+
     }
 
     /**
@@ -102,4 +119,6 @@ public class AMQPConsumerGui extends AMQPSamplerGui {
     protected void setMainPanel(JPanel panel) {
         mainPanel = panel;
     }
+
+
 }
