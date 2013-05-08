afcf7d1b1ffa420c0d2b81c7a23f02deab450398
Jeff Lavallee <jeff@zeroclue.com>
2013-05-08 13:31:42 -0700

    null out closed channel to force reopen

--- src/main/com/zeroclue/jmeter/protocol/amqp/AMQPSampler.java
+++ src/main/com/zeroclue/jmeter/protocol/amqp/AMQPSampler.java
@@ -70,6 +70,7 @@ public abstract class AMQPSampler extends AbstractSampler implements ThreadListe
         if(channel != null && !channel.isOpen()){
             log.warn("channel " + channel.getChannelNumber()
                     + " closed unexpectedly: ", channel.getCloseReason());
+            channel = null; // so we re-open it below
         }
         
         if(channel == null) {
