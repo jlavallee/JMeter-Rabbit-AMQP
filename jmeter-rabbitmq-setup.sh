a68bf6fc964f3de5bede4d2782d7e02229ebe49e
Wilson Mar <wilsonmar@gmail.com>
2018-01-08 08:40:30 -0500

    extras

--- jmeter-rabbitmq-setup.sh
+++ jmeter-rabbitmq-setup.sh
@@ -79,19 +79,27 @@ FOLDER="$JMETER_HOME/libexec/lib/ext"
 if [ -f $FOLDER/$FILE ]; then  # file exists within folder 
    fancy_echo "$FILE already installed in $FOLDER. Skipping install."
 else
-   fancy_echo "Downloading and moving $FILE to $FOLDER ..."
+   fancy_echo "Downloading $FILE to $FOLDER ..."
    # From https://jmeter-plugins.org/wiki/StandardSet/
    curl -O http://jmeter-plugins.org/downloads/file/jmeter-plugins-manager-0.18.jar
 
    fancy_echo "Moving $FILE to $FOLDER ..."
    cp $FILE  $FOLDER  -i
 fi
-exit
 
 
-# TODO: Check if version has changed.
-#  fancy_echo "Installing plugin extras to $JMETER_HOME/libexec/lib/ext ..."
-# curl -O http://jmeter-plugins.org/downloads/file/JMeterPlugins-Extras-1.2.1.zip
+FILE="JMeterPlugins-Extras-1.2.1.zip"  # TODO: Check if version has changed since Jan 4, 2018.
+FOLDER="$JMETER_HOME/libexec/lib/ext"
+if [ -f $FOLDER/$FILE ]; then  # file exists within folder 
+   fancy_echo "$FILE already installed in $FOLDER. Skipping install."
+else
+   fancy_echo "Downloading $FILE to $FOLDER ..."
+   # From https://jmeter-plugins.org/wiki/Extras
+   curl -O http://jmeter-plugins.org/downloads/file/JMeterPlugins-Extras-1.2.1.zip
+
+   fancy_echo "Moving $FILE to $FOLDER ..."
+   cp $FILE  $FOLDER  -i
+fi
 
 
 REPO1="JMeter-Rabbit-AMQP"
